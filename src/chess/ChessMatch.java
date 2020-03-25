1fdb095ec3322bea556a209a8a5df9b77662f342
nataliegusso <nataliegusso@gmail.com>
2020-03-25 10:12:51 -0300

    Exception and printing the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.Bishops;
 import chess.pieces.King;
 import chess.pieces.Knights;
@@ -30,7 +29,46 @@ public class ChessMatch {
 	return mat;			//Matriz de peças da partida
 	}
 
-	private void initialSetup() {  //Método que coloca as peças no tabuleiro
+	private void placeNewPiece(char column, int row, ChessPiece piece) {  //Passa as posições na coordenada do xadrez
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	private void initialSetup() {  //Método que coloca as peças no tabuleiro, coordenadas do xadrez
+		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('b', 8, new Knights(board, Color.BLACK));
+		placeNewPiece('c', 8, new Bishops(board, Color.BLACK));
+		placeNewPiece('d', 8, new Queen(board, Color.BLACK));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('f', 8, new Bishops(board, Color.BLACK));
+		placeNewPiece('g', 8, new Knights(board, Color.BLACK));
+		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('a', 7, new Pawns(board, Color.BLACK));
+		placeNewPiece('b', 7, new Pawns(board, Color.BLACK));
+		placeNewPiece('c', 7, new Pawns(board, Color.BLACK));
+		placeNewPiece('d', 7, new Pawns(board, Color.BLACK));
+		placeNewPiece('e', 7, new Pawns(board, Color.BLACK));
+		placeNewPiece('f', 7, new Pawns(board, Color.BLACK));
+		placeNewPiece('g', 7, new Pawns(board, Color.BLACK));
+		placeNewPiece('h', 7, new Pawns(board, Color.BLACK));
+
+		placeNewPiece('a', 2, new Pawns(board, Color.WHITE));
+		placeNewPiece('b', 2, new Pawns(board, Color.WHITE));
+		placeNewPiece('c', 2, new Pawns(board, Color.WHITE));
+		placeNewPiece('d', 2, new Pawns(board, Color.WHITE));
+		placeNewPiece('e', 2, new Pawns(board, Color.WHITE));
+		placeNewPiece('f', 2, new Pawns(board, Color.WHITE));
+		placeNewPiece('g', 2, new Pawns(board, Color.WHITE));
+		placeNewPiece('h', 2, new Pawns(board, Color.WHITE));
+		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('b', 1, new Knights(board, Color.WHITE));
+		placeNewPiece('c', 1, new Bishops(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+		placeNewPiece('e', 1, new Queen(board, Color.WHITE));
+		placeNewPiece('f', 1, new Bishops(board, Color.WHITE));
+		placeNewPiece('g', 1, new Knights(board, Color.WHITE));
+		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+	}
+	
+/*	private void initialSetup() {  //Método que coloca as peças no tabuleiro, coordenadas da matriz
 		board.placePiece(new Rook(board, Color.BLACK), new Position(0,0));
 		board.placePiece(new Knights(board, Color.BLACK), new Position(0,1));
 		board.placePiece(new Bishops(board, Color.BLACK), new Position(0,2));
@@ -64,7 +102,6 @@ public class ChessMatch {
 		board.placePiece(new Bishops(board, Color.WHITE), new Position(7,5));
 		board.placePiece(new Knights(board, Color.WHITE), new Position(7,6));
 		board.placePiece(new Rook(board, Color.WHITE), new Position(7,7));
-
-		
 	}
+*/
 }
